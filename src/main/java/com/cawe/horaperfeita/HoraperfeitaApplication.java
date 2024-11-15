8e5bffbab2c6d43c73986815e9ef1985569bdf2d
Cawe541 <cawe.wewe04@gmail.com>
2024-11-15 13:18:35 -0300

    init

---
+++ src/main/java/com/cawe/horaperfeita/HoraperfeitaApplication.java
@@ -0,0 +1,13 @@
+package com.cawe.horaperfeita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraperfeitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraperfeitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cawe/horaperfeita/HoraperfeitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cawe.horaperfeita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraperfeitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
