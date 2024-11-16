94ccc949cdf885c1e82894d72611e2f1bd385adf
Cawe541 <cawe.wewe04@gmail.com>
2024-11-16 09:45:30 -0300

    wf-1: feat: creating a base service class

---
+++ src/main/java/com/cawe/horaperfeita/application/services/BaseService.java
@@ -0,0 +1,41 @@
+package com.cawe.horaperfeita.application.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public abstract class BaseService<BaseEntity> {
+
+    public JpaRepository<BaseEntity, Long> repository;
+
+    public BaseService() {
+        this.repository = null;
+    }
+
+    @Autowired
+    public void setRepository(JpaRepository<BaseEntity, Long> repository) {
+        this.repository = repository;
+    }
+
+    public BaseEntity buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<BaseEntity> buscarTodos() {
+        return repository.findAll();
+    }
+
+    public BaseEntity criar(BaseEntity entity) {
+        return repository.save(entity);
+    }
+
+    public BaseEntity atualizar(BaseEntity entity) {
+        return repository.save(entity);
+    }
+
+    public void remover(Long id) {
+        repository.deleteById(id);
+    }
+
+}
