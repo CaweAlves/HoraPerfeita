5639c74d48d86c34893e49d2419efe1f6918a42d
Cawe541 <cawe.wewe04@gmail.com>
2024-11-20 09:49:08 -0300

    wf-1: refact: should be able to register a new user in the application

--- src/test/java/com/cawe/horaperfeita/application/controllers/AuthControllerTest.java
+++ src/test/java/com/cawe/horaperfeita/application/controllers/AuthControllerTest.java
@@ -1,52 +1,46 @@
 package com.cawe.horaperfeita.application.controllers;
 
 import com.cawe.horaperfeita.application.dtos.user.RegisterUserDTO;
-import com.cawe.horaperfeita.domain.entities.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
     @DisplayName("should be able to register a new user in the application")
-    public void shouldBeAbleToRegisterANewUserInTheApplication() throws JsonProcessingException {
-        User userForValidation = new User("Cawe", "cawe.alves", "contato@cawe.dev", "password");
+    public void shouldBeAbleToRegisterANewUserInTheApplication() throws Exception {
         RegisterUserDTO request = new RegisterUserDTO("Cawe", "cawe.alves", "contato@cawe.dev", "password");
 
         String url = "/auth/register";
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        HttpEntity<RegisterUserDTO> entity = new HttpEntity<>(request, headers);
-
-        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
-
-        String responseBody = response.getBody();
-        assertNotNull(responseBody);
-        assertTrue(responseBody.startsWith("{") && responseBody.endsWith("}"), "Response body is not JSON");
-
-        User createdUser = new ObjectMapper().readValue(response.getBody(), User.class);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(userForValidation.getName(), createdUser.getName());
-        assertEquals(userForValidation.getUsername(), createdUser.getUsername());
-        assertEquals(userForValidation.getEmail(), createdUser.getEmail());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name")
+                        .value(request.name()))
+                .andExpect(jsonPath("$.username")
+                        .value(request.username()))
+                .andExpect(jsonPath("$.email")
+                        .value(request.email()));
     }
 
 }
