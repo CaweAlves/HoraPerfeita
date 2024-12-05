4bb82d9814c27e9cacccad260ffa1f70c1a42a59
Cawe541 <cawe.wewe04@gmail.com>
2024-12-05 12:23:34 -0300

    wf-2: feat: creating weather data class

--- src/main/java/com/cawe/horaperfeita/application/services/WeatherService.java
+++ src/main/java/com/cawe/horaperfeita/application/services/WeatherService.java
@@ -1,6 +1,10 @@
 package com.cawe.horaperfeita.application.services;
 
+import com.cawe.horaperfeita.infrastructure.external.data.WeatherData;
 import com.cawe.horaperfeita.infrastructure.external.interfaces.IForecast;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +14,25 @@ import org.springframework.stereotype.Service;
 public class WeatherService {
 
     @Autowired
-    private final IForecast iForecast;
+    private final IForecast forecast;
 
-    public String getSevenDaysForecast(String latitude, String longitude) {
-        iForecast.addTemperature();
-        iForecast.addLatitudeAndLongitude(latitude, longitude);
-        return this.iForecast.getWeatherForecast();
+    public WeatherData getSevenDaysForecast(String latitude, String longitude) {
+        forecast.addTemperature();
+        forecast.addLatitudeAndLongitude(latitude, longitude);
+        String weatherForecast = this.forecast.getWeatherForecast();
+        return this.toWeatherData(weatherForecast);
     }
+
+    private WeatherData toWeatherData(String weatherForecast) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            WeatherData weatherData = objectMapper.readValue(weatherForecast, WeatherData.class);
+            return weatherData;
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
