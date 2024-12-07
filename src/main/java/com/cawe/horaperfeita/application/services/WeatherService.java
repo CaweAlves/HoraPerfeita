8709284661a0b1d7be2d556e238a791161aa5e86
Cawe541 <cawe.wewe04@gmail.com>
2024-12-07 12:18:42 -0300

    wf-2: feat(weather service): group time and temperature

--- src/main/java/com/cawe/horaperfeita/application/services/WeatherService.java
+++ src/main/java/com/cawe/horaperfeita/application/services/WeatherService.java
@@ -2,12 +2,13 @@ package com.cawe.horaperfeita.application.services;
 
 import com.cawe.horaperfeita.infrastructure.external.data.WeatherData;
 import com.cawe.horaperfeita.infrastructure.external.interfaces.IForecast;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +17,23 @@ public class WeatherService {
     @Autowired
     private final IForecast forecast;
 
-    public WeatherData getSevenDaysForecast(String latitude, String longitude) {
+    public Map<String, Double> getSevenDaysForecast(String latitude, String longitude) {
         forecast.addTemperature();
         forecast.addLatitudeAndLongitude(latitude, longitude);
-        return forecast.getWeatherForecast();
+        return this.groupTimeAndTemperature(forecast.getWeatherForecast());
+    }
 
+    public Map<String, Double> groupTimeAndTemperature(WeatherData weatherForecast) {
+        List<String> times = weatherForecast.getHourly().getTime();
+        List<Double> temperatures = weatherForecast.getHourly().getTemperature2m();
+
+        Map<String, Double> days = new HashMap<>();
+
+        for (Integer count = 0; count < times.stream().count(); count++) {
+            days.put(times.get(count), temperatures.get(count));
+        }
+
+        return days;
     }
 
 }
